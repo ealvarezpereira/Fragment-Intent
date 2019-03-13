8b50b2d20ef804152f19c453e98aa80ebd4d13ce
ealvarezpereira <ealvarezpereira@danielcastelao.org>
2019-03-13 10:36:32 +0100

    Proyecto con Fragment e Intent

--- app/src/main/java/com/nr1k/kayku/verticalhorizontal/Activity2.kt
+++ app/src/main/java/com/nr1k/kayku/verticalhorizontal/Activity2.kt
@@ -1,30 +1,50 @@
 package com.nr1k.kayku.verticalhorizontal
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.toast
+import kotlinx.android.synthetic.main.activity2.*
+
+import org.json.JSONObject
+import android.R.attr.data
+import android.webkit.WebView
+
 
 
 class Activity2 : AppCompatActivity() {
 
     var horizontal = false
+    var valor = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
-        val valor = intent.getIntExtra("id",0)
-        toast(valor.toString())
-        //dialContactPhone(valor.toString())
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.frankmento, FrankMento.newInstance(), "Frankmento")
-                .commit()
-        }
+        valor = intent.getStringExtra("id")
+        metodoTitulo()
+        parseJson()
+        //if (savedInstanceState == null) {
+        //    supportFragmentManager
+        //        .beginTransaction()
+        //        .add(R.id.frankmento, FrankMento.newInstance(), "Frankmento")
+        //        .commit()
+        //}
     }
-    private fun dialContactPhone(phoneNumber: String) {
-        startActivity(Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)))
+
+
+    fun parseJson(){
+
+        val jsonObj = JSONObject(valor)
+        val jSonObjTitulo = jsonObj.getJSONObject("content")
+        val strTitulo = jSonObjTitulo.getString("rendered")
+        val webview = webview as WebView
+        webview.settings.javaScriptEnabled = true
+        webview.loadDataWithBaseURL("", strTitulo, "text/html", "UTF-8", "")
+    }
+
+
+    fun metodoTitulo(){
+
+        val jsonObj = JSONObject(valor)
+        val jSonObjTitulo = jsonObj.getJSONObject("title")
+        val strTitulo = jSonObjTitulo.getString("rendered")
+        textotweet.text = strTitulo
     }
 }
--- app/src/main/java/com/nr1k/kayku/verticalhorizontal/MainActivity.kt
+++ app/src/main/java/com/nr1k/kayku/verticalhorizontal/MainActivity.kt
@@ -3,19 +3,19 @@ package com.nr1k.kayku.verticalhorizontal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     var horizontal = false
+    var resp = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
+        peticionwp()
         if(id_horizontal != null){
 
             horizontal = true
@@ -51,7 +51,24 @@ class MainActivity : AppCompatActivity() {
                 //startActivity(intent)
 
                 //Inicia la actividad pero simplificado con anko
-                startActivity(intentFor<Activity2>("id" to 663533850))
+
+
+
+                startActivity(intentFor<Activity2>("id" to resp))
+            }
+        }
+    }
+
+    fun peticionwp() {
+        var respuesta = ""
+        // lanza la corutina NO en el hilo principal
+        doAsync{
+            // peticion a wordpress
+            respuesta = URL("http://18.221.53.69/wp5/?rest_route=/wp/v2/posts/103").readText()
+            // Accedemos al hilo principal
+            uiThread {
+                resp = respuesta
+                longToast("Request performed")
             }
         }
     }
